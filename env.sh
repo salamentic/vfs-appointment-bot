4c42e98cabe6085087117ac90c933427e9bff7fe
salamentic <raniruddh99@gmail.com>
2024-08-17 15:29:56 -0400

    Added config file and env.sh that works for deployments, added dockerfile

---
+++ env.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Default Configuration
+export VFS_INTERVAL=1800
+
+# Browser Configuration
+export VFS_BROWSER="chromium"
+export VFS_HEADLESS=false
+
+# Notification Configuration
+export VFS_CHANNELS="your_notification_channels_here"
+
+# VFS Credential
+export VFS_EMAIL="your_vfs_email_here"
+export VFS_PASSWORD="your_vfs_password_here"
+
+# Discord Configuration
+export DISCORD_URL="your_discord_webhook_url_here"
+export DISCORD_USERNAME="your_discord_username_here"
+
+# Telegram Configuration
+export TELEGRAM_CHAT_ID="your_telegram_chat_id_here"
+export TELEGRAM_PARSE_MODE="your_telegram_parse_mode_here"
+export TELEGRAM_BOT_TOKEN="your_telegram_bot_token_here"
+
+# Twilio Configuration
+export TWILIO_URL="your_twilio_url_here"
+export TWILIO_AUTH_TOKEN="your_twilio_auth_token_here"
+export TWILIO_ACCOUNT_SID="your_twilio_account_sid_here"
+export TWILIO_SMS_ENABLED=true
+export TWILIO_CALL_ENABLED=false
+export TWILIO_TO_NUM="your_twilio_to_number_here"
+export TWILIO_FROM_NUM="your_twilio_from_number_here"
+
+# Email Notification Configuration
+export VFS_NOTIF_EMAIL="your_email_here"
+export VFS_NOTIF_PASSWORD="your_email_password_here"
